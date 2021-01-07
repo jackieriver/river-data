74b63e6fc0487cb6666df138687cdded257ba9ab
hepengfei <hpf@gfloan.com>
2021-01-07 22:52:16 +0800

    feat(): amq 有个bug,数据源不能一直保持监听

--- src/main/java/com/river/flink/Demo/mq/DataStreamAMQConsumer.java
+++ src/main/java/com/river/flink/Demo/mq/DataStreamAMQConsumer.java
@@ -9,10 +9,11 @@ import org.apache.flink.streaming.connectors.activemq.internal.RunningChecker;
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
 
 /**
- * @description:
+ * @description: 有个bug,不知道为什么不能一直监听着队列的消息
  * @author: He Pengfei
  * @time: 2021/1/7 9:49
  */
+@Deprecated
 public class DataStreamAMQConsumer {
 
     public static void main(String[] args) throws Exception{
